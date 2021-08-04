73436c6ed33fb32a6ac5d625bcb659424b782d50
yastalby <yasmine.talby@outlook.fr>
2021-08-04 09:58:50 -0700

    Create faculty-listing.php
    Listing of faculty users (for now display administrator user for testing purpose)

---
+++ templates/faculty-listing.php
@@ -0,0 +1,31 @@
+<?php /* Template Name: Faculty Listing */ ?>
+
+<?php get_header();?>
+<main id="main">
+<header class="header">
+<div class = "ucla campus">
+<div class= "col span_12_of_12">
+<br>
+<h2 class="yellow-side-header">Faculty</h2>
+</div>
+</div>
+</header>
+<div class = "ucla campus">
+<?php
+
+$args= array(
+        'role' => 'administrator',
+        'orderby'=> 'user_nicename',
+        'order' => 'ASC'
+);
+$users=get_users($args);
+echo '<ul style="list-style:none;">';
+foreach ( $users as $user ) {
+?><hr> <?php  echo '<li>' . esc_html( $user->display_name ) . '  Email: ' . esc_html( $user->user_email ) . '</li>';
+}
+echo '</ul>';
+?>
+<hr>
+</div>
+</main>
+<?php get_footer();?>
