8e0578313fb55f139667ef7c06366965f9e1611a
yastalby <yasmine.talby@outlook.fr>
2021-08-10 00:17:23 -0400

    creating template for home page

---
+++ templates/home-template.php
@@ -0,0 +1,14 @@
+<?php  /* Template Name: Home Template*/
+ get_header(); ?>
+
+<main id="main">
+ <header class="header">
+      <div class="ucla campus">
+	<div class="col span_12_of_12">    
+ </div> </div>
+    </header>
+<hr/>
+
+</main>
+
+<?php get_footer(); ?>
