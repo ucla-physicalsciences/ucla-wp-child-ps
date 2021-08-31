d1f4d59a38e65b1aec3d189b3c3e1c5e39f14a99
Yasmine Talby <yasmine.talby@outlook.fr>
2021-08-30 23:16:29 -0400

    General template -Yasmine

---
+++ templates/statement-template.php
@@ -0,0 +1,24 @@
+<?php /* Template Name: Statement Template */ ?>
+<?php get_header(); ?>
+<main id="main"> 
+    <header class="header" >
+      <div class="ucla campus">
+        <div class="col span_12_of_12"><br>
+  <div class="breadcrumb"><h2 class="yellow-side-header"><?php the_title(); ?></h2> 
+</div>
+      </div>
+    </header>
+    <div class="ucla campus">
+
+      <div class="col span_9_of_12">
+  <p>      <?php the_content(); ?></p>
+
+       
+      </div>
+
+    
+  </div>
+
+</main>
+
+<?php get_footer(); ?>
