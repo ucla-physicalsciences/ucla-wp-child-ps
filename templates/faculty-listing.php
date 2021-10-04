3ccddd49b3bde194586b7d99cbef6db55cffca30
Yasmine Talby <yasmine.talby@outlook.fr>
2021-10-03 20:21:59 -0400

    faculty general category re-added + reordering faculty listing

--- templates/faculty-listing.php
+++ templates/faculty-listing.php
@@ -14,48 +14,6 @@
 </header>
 <div class = "ucla campus">
 
-<!--Faculty Adjunct-->
-<div class="accordion accordion--card-content accordion--mobile-only">
-<dl>
-<button class="accordion__title" aria-expanded="false">
-<dt>Faculty Adjunct</dt>
-</button>
-<dd class="accordion__content">
-<?php
-
-$args= array(
-        'role' => 'faculty_adjunct',
-        'orderby'=> 'user_nicename',
-        'order' => 'ASC'
-);
-$users=get_users($args);
-echo '<ul style="list-style:none;">';
-foreach ( $users as $user ) {
-        wp_get_current_user();
-?><div class="col span_5_of_12"> <?php  echo '<li>'?>
-<article class="person-card">
-
-<img class="person-card__image" src= "<?php echo esc_url( get_avatar_url( $user->ID ) );?>" alt="Headshot of Faculty Member">
-<div class="person-card__info-wrapper">
-<h1 class="person-card__name"><a href="<?php get_author_link(true,$user->ID)?>"><span><?php echo esc_html($user->display_name);?></span></a></h1>
-<h2 class="person-card__department"><span><?php
-        global $wp_roles;
-        if (!empty($user->roles)){
-                foreach ($user->roles as $role){
-                echo $wp_roles->roles[ $role ]['name'] . ' ';}
-
-                };?></span></h2>
-        <p class="person-card__description"><?php echo esc_html($user->description);?></p>
-    </div>
-</article>
-<?php '</li>';
-?></div><?php }
-echo '</ul>';
-?>
-</dd>
-</dl>
-</div>
-<!--End Faculty Adjunct-->
 <!--Faculty Chair-->
 <!--End Faculty Chair-->
 <div class="accordion accordion--card-content accordion--mobile-only">
@@ -98,48 +56,6 @@ echo '</ul>';
 </dd>
 </dl>
 </div>
-<!--Faculty Emeritus-->
-<div class="accordion accordion--card-content accordion--mobile-only">
-<dl>
-<button class="accordion__title" aria-expanded="false">
-<dt>Faculty Emeritus</dt>
-</button>
-<dd class="accordion__content">
-<?php
-
-$args= array(
-        'role' => 'faculty_emeritus',
-        'orderby'=> 'user_nicename',
-        'order' => 'ASC'
-);
-$users=get_users($args);
-echo '<ul style="list-style:none;">';
-foreach ( $users as $user ) {
-        wp_get_current_user();
-?><div class="col span_5_of_12"> <?php  echo '<li>'?>
-<article class="person-card">
-
-<img class="person-card__image" src= "<?php echo esc_url( get_avatar_url( $user->ID ) );?>" alt="Headshot of Faculty Member">
-<div class="person-card__info-wrapper">
-<h1 class="person-card__name"><a href="<?php get_author_link(true,$user->ID)?>"><span><?php echo esc_html($user->display_name);?></span></a></h1>
-<h2 class="person-card__department"><span><?php
-        global $wp_roles;
-        if (!empty($user->roles)){
-                foreach ($user->roles as $role){
-                echo $wp_roles->roles[ $role ]['name'] . ' ';}
-
-                };?></span></h2>
-        <p class="person-card__description"><?php echo esc_html($user->description);?></p>
-    </div>
-</article>
-<?php '</li>';
-?></div><?php }
-echo '</ul>';
-?>
-</dd>
-</dl>
-</div>
-<!--End Faculty Emeritus-->
 <!--Faculty Vice Chair-->
 <div class="accordion accordion--card-content accordion--mobile-only">
 <dl>
@@ -182,7 +98,132 @@ echo '</ul>';
 </dl>
 </div>
 <!--End Faculty Vice Chair-->
+<!--Faculty General-->
+<div class="accordion accordion--card-content accordion--mobile-only">
+<dl>
+<button class="accordion__title" aria-expanded="false">
+<dt>Faculty General</dt>
+</button>
+<dd class="accordion__content">
+<?php
 
+$args= array(
+        'role' => 'faculty_general',
+        'orderby'=> 'user_nicename',
+        'order' => 'ASC'
+);
+$users=get_users($args);
+echo '<ul style="list-style:none;">';
+foreach ( $users as $user ) {
+        wp_get_current_user();
+?><div class="col span_5_of_12"> <?php  echo '<li>'?>
+<article class="person-card">
+
+<img class="person-card__image" src= "<?php echo esc_url( get_avatar_url( $user->ID ) );?>" alt="Headshot of Faculty Member">
+<div class="person-card__info-wrapper">
+<h1 class="person-card__name"><a href="<?php get_author_link(true,$user->ID)?>"><span><?php echo esc_html($user->display_name);?></span></a></h1>
+<h2 class="person-card__department"><span><?php
+        global $wp_roles;
+        if (!empty($user->roles)){
+                foreach ($user->roles as $role){
+                echo $wp_roles->roles[ $role ]['name'] . ' ';}
+
+                };?></span></h2>
+        <p class="person-card__description"><?php echo esc_html($user->description);?></p>
+    </div>
+</article>
+<?php '</li>';
+?></div><?php }
+echo '</ul>';
+?>
+</dd>
+</dl>
+</div>
+<!--End Faculty General-->
+<!--Faculty Adjunct-->
+<div class="accordion accordion--card-content accordion--mobile-only">
+<dl>
+<button class="accordion__title" aria-expanded="false">
+<dt>Faculty Adjunct</dt>
+</button>
+<dd class="accordion__content">
+<?php
+
+$args= array(
+        'role' => 'faculty_adjunct',
+        'orderby'=> 'user_nicename',
+        'order' => 'ASC'
+);
+$users=get_users($args);
+echo '<ul style="list-style:none;">';
+foreach ( $users as $user ) {
+        wp_get_current_user();
+?><div class="col span_5_of_12"> <?php  echo '<li>'?>
+<article class="person-card">
+
+<img class="person-card__image" src= "<?php echo esc_url( get_avatar_url( $user->ID ) );?>" alt="Headshot of Faculty Member">
+<div class="person-card__info-wrapper">
+<h1 class="person-card__name"><a href="<?php get_author_link(true,$user->ID)?>"><span><?php echo esc_html($user->display_name);?></span></a></h1>
+<h2 class="person-card__department"><span><?php
+        global $wp_roles;
+        if (!empty($user->roles)){
+                foreach ($user->roles as $role){
+                echo $wp_roles->roles[ $role ]['name'] . ' ';}
+
+                };?></span></h2>
+        <p class="person-card__description"><?php echo esc_html($user->description);?></p>
+    </div>
+</article>
+<?php '</li>';
+?></div><?php }
+echo '</ul>';
+?>
+</dd>
+</dl>
+</div>
+<!--End Faculty Adjunct-->
+<!--Faculty Emeritus-->
+<div class="accordion accordion--card-content accordion--mobile-only">
+<dl>
+<button class="accordion__title" aria-expanded="false">
+<dt>Faculty Emeritus</dt>
+</button>
+<dd class="accordion__content">
+<?php
+
+$args= array(
+        'role' => 'faculty_emeritus',
+        'orderby'=> 'user_nicename',
+        'order' => 'ASC'
+);
+$users=get_users($args);
+echo '<ul style="list-style:none;">';
+foreach ( $users as $user ) {
+        wp_get_current_user();
+?><div class="col span_5_of_12"> <?php  echo '<li>'?>
+<article class="person-card">
+
+<img class="person-card__image" src= "<?php echo esc_url( get_avatar_url( $user->ID ) );?>" alt="Headshot of Faculty Member">
+<div class="person-card__info-wrapper">
+<h1 class="person-card__name"><a href="<?php get_author_link(true,$user->ID)?>"><span><?php echo esc_html($user->display_name);?></span></a></h1>
+<h2 class="person-card__department"><span><?php
+        global $wp_roles;
+        if (!empty($user->roles)){
+                foreach ($user->roles as $role){
+                echo $wp_roles->roles[ $role ]['name'] . ' ';}
+
+                };?></span></h2>
+        <p class="person-card__description"><?php echo esc_html($user->description);?></p>
+    </div>
+</article>
+<?php '</li>';
+?></div><?php }
+echo '</ul>';
+?>
+</dd>
+</dl>
+</div>
+<!--End Faculty Emeritus-->
 
 </div>
 </main>
