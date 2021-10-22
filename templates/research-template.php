b34fc06170eab2a7cb6f551eca563a1a29dc6f54
Yasmine Talby <yasmine.talby@outlook.fr>
2021-10-22 16:07:05 -0400

    Trying different components & layouts

--- templates/research-template.php
+++ templates/research-template.php
@@ -2,11 +2,11 @@
 <?php get_header();?>
 <main id="main">
 <header class="header">
-<div class = "ucla campus">
+<div id="academics-college-and-schools" class = "ucla campus">
 <div class= "col span_12_of_12">
 <br>
-<h2 class="yellow-side-header">Research</h2><br><br>
-<p class="lead"> Introductory description of the Research Menu section </p><p>Descriptive text are type 2 for more detailed informations on this section</p>
+<h2 class="yellow-side-header">AOS Research Overview</h2><br><br>
+<p class="lead"> Areas of Research </p><p>The UCLA Department of Atmospheric and Oceanic Sciences offers five areas of research: Climate &amp; Weather, Chemistry &amp; Radiation, Oceanography, Space Physics, and Biogeochemistry.</p>
 </div>
 </div>
 </header>
@@ -14,12 +14,239 @@
 Second ligne: Oceanography; Space Physics-->
 <h3 class="pt-24 pb-12"></h3>
 <div class="ucla campus">
+    <div class="pt-0 mb-n58 pb-lg-64  pb-md-64  mb-32 section-wrapper school-card__section">
+
+        <div class="mb-5 school-card__wrapper">
+
+            <div class="pb-lg-3 ucla campus school-card__ribbon">
+                <a href="http://aos.ucla.edu/?page_id=38">
+                    <div class="col span_4_of_12 mt-n3 mb-lg-0 mb-3 school-card__img">
+                        <img src="http://aos.ucla.edu/wp-content/uploads/2021/10/Biogeochemistry2.png" class="img--retina img--wide" alt="Antartica ocean figure" />
+                    </div>
+                    <div class="mt-lg-32 col mb-0 span_5_of_12 school-card__title">
+                        <span><span><h2 class="pb-0">Biogeochemistry</h2></span></span>
+       <br>             </div>
+                </a>
+
+                <div class="col span_5_of_12 school-card__info">
+
+                    <div class="school-card__info-container">
+
+                        <div class="pb-lg-3 pb-2 school-card__degree">
+
+                            <span class="school-card__degree"><b>Offers B.S., M.S., Ph.D.</b></span>
+
+                        </div>
+
+                        <div class="p-0 school-card__content">
+                            <p class="pb-0 mb-3 mb-md-4 mb-lg-0 school-card__description">
+                                Geophysical fluid dynamics; ocean and climate modeling.  Geophysical fluid dynamics and ocean-ice interactions. Marine biogeochemistry and geomicrobiology.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+ <div class="pt-0 mb-n58 pb-lg-64  pb-md-64  mb-32 section-wrapper school-card__section ">
+        <div class="mb-5 school-card__wrapper">
+            <div class="pb-lg-3 ucla campus school-card__ribbon">
+                <a href="http://aos.ucla.edu/?page_id=38">
+                    <div class="col span_4_of_12 mt-n3 mb-lg-0 mb-3 school-card__img">
+                        <img src="http://aos.ucla.edu/wp-content/uploads/2021/10/Atmospheric_Chemistry7.jpg" class="img--retina img--wide" alt="Antartica ocean figure" />
+                    </div>
+                    <div class="mt-lg-32 col mb-0 span_5_of_12 school-card__title">
+                        <span><span><h2 class="pb-0">Chemistry and Radiation</h2></span></span>
+       <br>             </div>
+                </a>
+
+                <div class="col span_5_of_12 school-card__info">
+
+                    <div class="school-card__info-container">
+
+                        <div class="pb-lg-3 pb-2 school-card__degree">
+
+                            <span class="school-card__degree"><b>Offers B.S., M.S., Ph.D.</b></span>
+
+                        </div>
+
+                        <div class="p-0 school-card__content">
+                            <p class="pb-0 mb-3 mb-md-4 mb-lg-0 school-card__description">
+                                The Department of Atmospheric and Oceanic Sciences has a longstanding tradition of experimental and theoretical research in atmospheric chemistry and physics. The department is indeed built around a 9 story high precipitation shaft used by Hans Pruppacher to study the growth of water drops.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+ <div class="pt-0 mb-n58 pb-lg-64  pb-md-64  mb-32 section-wrapper school-card__section">
+
+        <div class="mb-5 school-card__wrapper">
+
+            <div class="pb-lg-3 ucla campus school-card__ribbon">
+                <a href="http://aos.ucla.edu/?page_id=38">
+                    <div class="col span_4_of_12 mt-n3 mb-lg-0 mb-3 school-card__img">
+                        <img src="http://aos.ucla.edu/wp-content/uploads/2021/10/Atmospheric_Dynamics3.png" class="img--retina img--wide pt-40" alt="Antartica ocean figure" />
+                    </div>
+                    <div class="mt-lg-32 col mb-0 span_5_of_12 school-card__title">
+                        <span><span><h2 class="pb-0">Dynamics and Climate</h2></span></span>
+       <br>             </div>
+                </a>
+
+                <div class="col span_5_of_12 school-card__info">
+
+                    <div class="school-card__info-container">
+
+                        <div class="pb-lg-3 pb-2 school-card__degree">
+
+                            <span class="school-card__degree"><b>Offers B.S., M.S., Ph.D.</b></span>
+
+                        </div>
+
+                        <div class="p-0 school-card__content">
+                            <p class="pb-0 mb-3 mb-md-4 mb-lg-0 school-card__description">
+                                UCLA is a world-class center for weather and climate research. Over the past 40 years, UCLA researchers have made historic contributions to our understanding of the fluid dynamics of the atmosphere and oceans, teleconnections between weather regimes all over the globe, and the development of techniques to simulate weather and climate.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+ <div class="pt-0 mb-n58 pb-lg-64  pb-md-64  mb-32 section-wrapper school-card__section">
+
+        <div class="mb-5 school-card__wrapper">
+
+            <div class="pb-lg-3 ucla campus school-card__ribbon">
+                <a href="http://aos.ucla.edu/?page_id=38">
+                    <div class="col span_4_of_12 mt-n3 mb-lg-0 mb-3 school-card__img">
+                        <img src="http://aos.ucla.edu/wp-content/uploads/2021/10/Oceanography3.jpg" class="img--retina img--wide" alt="Antartica ocean figure" />
+                    </div>
+                    <div class="mt-lg-32 col mb-0 span_5_of_12 school-card__title">
+                        <span><span><h2 class="pb-0">Oceanography</h2></span></span>
+       <br>             </div>
+                </a>
+
+                <div class="col span_5_of_12 school-card__info">
+
+                    <div class="school-card__info-container">
+
+                        <div class="pb-lg-3 pb-2 school-card__degree">
+
+                            <span class="school-card__degree"><b>Offers B.S., M.S., Ph.D.</b></span>
+
+                        </div>
+
+                        <div class="p-0 school-card__content">
+                            <p class="pb-0 mb-3 mb-md-4 mb-lg-0 school-card__description">
+                                Geophysical fluid dynamics; ocean and climate modeling.  Geophysical fluid dynamics and ocean-ice interactions. Marine biogeochemistry and geomicrobiology.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+  <div class="pt-0 mb-n58 pb-lg-64  pb-md-64  mb-32 section-wrapper school-card__section">
+        <div class="mb-5 school-card__wrapper">
+
+            <div class="pb-lg-3 ucla campus school-card__ribbon">
+                <a href="http://aos.ucla.edu/?page_id=38">
+                    <div class="col span_4_of_12 mt-n3 mb-lg-0 mb-3 school-card__img">
+                        <img src="http://aos.ucla.edu/wp-content/uploads/2021/10/Planetary_Atmospheres.png" class="img--retina img--wide pl-40" alt="Antartica ocean figure" />
+                        <img src="http://aos.ucla.edu/wp-content/uploads/2021/10/Space_Physics.jpg" class="img--retina img--wide" alt="Antartica ocean figure" />
+                    </div>
+                    <div class="mt-lg-32 col mb-0 span_5_of_12 school-card__title">
+                        <span><span><h2 class="pb-0">Space Physics</h2></span></span>
+       <br>             </div>
+                </a>
+
+                <div class="col span_5_of_12 school-card__info">
+
+                    <div class="school-card__info-container">
+
+                        <div class="pb-lg-3 pb-2 school-card__degree">
+
+                            <span class="school-card__degree"><b>Offers B.S., M.S., Ph.D.</b></span>
+
+                        </div>
+
+                        <div class="p-0 school-card__content">
+                            <p class="pb-0 mb-3 mb-md-4 mb-lg-0 school-card__description">
+				Above 100 km altitude, the Earth’s atmosphere becomes highly ionized, collisions cease to be important, and dynamics are largely controlled by the terrestrial magnetic field. This field is distorted by the continuous flow of solar wind plasma from the Sun into a cavity know as the magnetosphere, which forms the uppermost region of the Earth’s atmosphere.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+<article class="basic-card-grey">
+  <img class="basic-card__image" src="http://aos.ucla.edu/wp-content/uploads/2021/10/Biogeochemistry.jpg" alt="Image related to field description">
+  <div class="basic-card__info-wrapper">
+    <h1 class="basic-card__title" href="http://aos.ucla.edu/biogeochemistry/"><span>Biogeochemistry</span></h1>
+    <p class="basic-card__description">Biogeochemistry descriptive text to be provided</p>
+    <div class="basic-card__buttons">
+      <button class="btn btn--tertiary">
+        Faculty
+      </button>
+     
+    </div>
+  </div>
+</article>
+<article class="basic-card">
+  <img class="basic-card__image" src="http://aos.ucla.edu/wp-content/uploads/2021/10/Atmospheric_Chemistry7.jpg" alt="Image related to field description">
+  <div class="basic-card__info-wrapper">
+    <h1 class="basic-card__title"><span>Chemistry and Radiation</span></h1>
+    <p class="basic-card__description">The Department of Atmospheric and Oceanic Sciences has a longstanding tradition of experimental and theoretical research in atmospheric chemistry and physics. The department is indeed built around a 9 story high precipitation shaft used by Hans Pruppacher to study the growth of water drops.</p>
+    <div class="basic-card__buttons">
+      <button class="btn btn--tertiary">
+        Faculty
+      </button>
+    </div>
+  </div>
+</article>
+
+<article class="basic-card-grey">
+  <img class="basic-card__image" src="http://aos.ucla.edu/wp-content/uploads/2021/10/Atmospheric_Dynamics3.png" alt="Image related to field description">
+  <div class="basic-card__info-wrapper">
+    <h1 class="basic-card__title" href="http://aos.ucla.edu/climate-weather/"><span></span>Dynamics and Climate</h1>
+    <p class="basic-card__description">UCLA is a world-class center for weather and climate research. Over the past 40 years, UCLA researchers have made historic contributions to our understanding of the fluid dynamics of the atmosphere and oceans, teleconnections between weather regimes all over the globe, and the development of techniques to simulate weather and climate.</p>
+    <div class="basic-card__buttons">
+      <button class="btn btn--tertiary">
+        Faculty
+      </button>
+    </div>
+  </div>
+</article>
+<article class="basic-card">
+  <img class="basic-card__image" src="http://aos.ucla.edu/wp-content/uploads/2021/10/Oceanography.png" alt="Image related to field description">
+  <div class="basic-card__info-wrapper">
+    <h1 class="basic-card__title" href="https://aos.ucla.edu/oceanography/"><span></span>Oceanography</h1>
+    <p class="basic-card__description">The still largely unexplored vastness of the ocean, covering about 2/3 of the surface of earth and with an average depth of almost 4 kilometers, has fascinated humans for as long as we have records for. Its currents, chemical composition, and ecosystems are all highly variable at different locations and times.</p>
+    <div class="basic-card__buttons">
+      <button class="btn btn--tertiary">
+        Faculty
+      </button>
+    </div>
+  </div>
+</article>
+<article class="basic-card-grey">
+  <img class="basic-card__image" src="http://aos.ucla.edu/wp-content/uploads/2021/10/Planetary_Atmospheres.png" alt="Image related to field description">
+  <div class="basic-card__info-wrapper">
+    <h1 class="basic-card__title"href="http://aos.ucla.edu/space-physics/"><span>Space Physics</span></h1>
+    <p class="basic-card__description">Above 100 km altitude, the Earth’s atmosphere becomes highly ionized, collisions cease to be important, and dynamics are largely controlled by the terrestrial magnetic field. This field is distorted by the continuous flow of solar wind plasma from the Sun into a cavity know as the magnetosphere, which forms the uppermost region of the Earth’s atmosphere.</p>
+    <div class="basic-card__buttons">
+      <button class="btn btn--tertiary">
+        Faculty
+      </button>
+    </div>
+  </div>
+</article></div><br><br>
+<div class="ucla campus">
   <section class="tile__section tile__section--col-4">
-    <span class="tile tile__background">
-      <a class="tile__link" href="http://aos.ucla.edu/aos-research-overview/">
-        <h3 class="tile__title">AOS Research Overview</h3>
-      </a>
-    </span>
     <span class="tile tile__background ">
       <a class="tile__link" href="http://aos.ucla.edu/biogeochemistry/">
         <h3 class="tile__title">Biogeochemistry</h3>
@@ -29,12 +256,12 @@ Second ligne: Oceanography; Space Physics-->
       <a class="tile__link" href="http://aos.ucla.edu/chemistry-radiation/">
         <h3 class="tile__title">Chemistry & Radiation</h3>
       </a>
-    </span>
+    </span> 
     <span class="tile tile__background ">
       <a class="tile__link" href="http://aos.ucla.edu/climate-weather/">
         <h3 class="tile__title">Dynamics and Climate</h3>
       </a>
-    </span>
+    </span> 
     <span class="tile tile__background">
       <a class="tile__link" href="https://aos.ucla.edu/oceanography/">
         <h3 class="tile__title">Oceanography</h3>
