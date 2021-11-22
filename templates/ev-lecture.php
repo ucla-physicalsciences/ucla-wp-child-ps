f92f67b4463480648138ae512ce57f26a0a4ce09
Yasmine Talby <yasmine.talby@outlook.fr>
2021-11-22 13:30:26 -0500

    update listing pages

--- templates/ev-lecture.php
+++ templates/ev-lecture.php
@@ -24,22 +24,22 @@ $args = array (
                 'orderby'       => 'date',
                 );
 $the_query = new WP_Query( $args );?>
-<section class="story">
-  <div class="story__secondary section group">
-<?php
-    if ( $the_query->have_posts() ) : ?>
+<section>
+	 <?php
+if ( $the_query->have_posts() ) : ?>
+
         <?php while ( $the_query->have_posts() ) : $the_query->the_post();
     $thumbnail_other = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );?>
-    <article class="story__secondary-card">
-    <a href="<?php echo the_permalink(); ?>">
-    <div class="story__secondary-image-wrapper"><img class="story__secondary-image" src="<?php echo $thumbnail_other[0]?>" /></div>
-        <h1 class="story__secondary-title">
-        <span class="story__secondary-title-text"><?php echo the_title();?></span>
-        </h1>
-      </a>
-      <div class="story__secondary-content">
-        <p class="story__secondary-blurb"><b>Speaker: </b><?php if(get_post_meta($post->ID, 'event_speaker', true)):
-	echo get_post_meta($post->ID, 'event_speaker', true);endif;?><br><b>Institution:</b>
+    <article class="basic-card">
+    <a  href="<?php echo the_permalink(); ?>" >
+    <img class="basic-card-image" src="<?php echo $thumbnail_other[0]?>" />
+        <h3 class="basic-card__title">
+        <span class="basic-card__title"><?php echo the_title();?></span>
+        </h3>
+  </a>
+<div class="basic-card__infor-wrapper">
+        <p class="basic-card__description"><b>Speaker: </b><?php if(get_post_meta($post->ID, 'event_speaker', true)):
+        echo get_post_meta($post->ID, 'event_speaker', true);endif;?><br><b>Institution:</b>
 <?php if(get_post_meta($post->ID, 'event_institution', true)):?>
         <?php
           echo get_post_meta($post->ID, 'event_institution', true);
@@ -56,7 +56,7 @@ echo get_post_meta($post->ID, 'event_date', true);endif;?> from <?php if(get_pos
 echo get_post_meta($post->ID, 'event_time', true);
 endif;?>
 </p>
-        <p class="story__secondary-source"></p>
+
       </div>
     </article>
         <?php endwhile; ?>
@@ -64,7 +64,8 @@ endif;?>
         <?php wp_reset_postdata(); ?>
 
      <?php endif; ?>
-</div></section>
+</section>
+
 </div>
 </main>
 <?php get_footer();?>
--- templates/newsletters-listing-template.php
+++ templates/newsletters-listing-template.php
@@ -24,24 +24,22 @@ $args = array (
                 'orderby'       => 'date',
                 );
 $the_query = new WP_Query( $args );?>
-<section class="story">
-  <div class="story__secondary section group">
+<section>
 <?php
     if ( $the_query->have_posts() ) : ?>
         <?php while ( $the_query->have_posts() ) : $the_query->the_post();
     $thumbnail_other = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );?>
-    <article class="story__secondary-card">
+    <article class="basic-card-grey">
     <a href="<?php echo the_permalink(); ?>">
-    <div class="story__secondary-image-wrapper"><img class="story__secondary-image" src="<?php echo $thumbnail_other[0]?>" /></div>
-        <h1 class="story__secondary-title">
-        <span class="story__secondary-title-text"><?php echo the_title();?></span>
-        </h1>
-      </a>
-      <div class="story__secondary-content">
-        <p class="story__secondary-blurb"><?php if(get_post_meta($post->ID, 'summary_news', true)):
+    <img class="basic-card__image" src="<?php echo $thumbnail_other[0]?>" />
+</a>
+      <div class="basic-card__info-wrapper">
+         <h3 class="basic-card__title">
+        <span  class="basic-card__title"><?php echo the_title();?></span>
+        </h3>
+      <p class="basic-card__description"><?php if(get_post_meta($post->ID, 'summary_news', true)):
           echo get_post_meta($post->ID, 'summary_news', true);
 endif;?></p>
-        <p class="story__secondary-source"></p>
       </div>
     </article>
         <?php endwhile; ?>
@@ -49,7 +47,7 @@ endif;?></p>
         <?php wp_reset_postdata(); ?>
 
      <?php endif; ?>
-</div></section>
+</section>
 </div>
 </main>
 <?php get_footer();?>
