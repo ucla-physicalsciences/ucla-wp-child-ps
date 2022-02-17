9253bd57983bab9cce376fa92bb2d226c40a924e
Yasmine Talby <yasmine.talby@outlook.fr>
2022-02-17 12:35:29 -0500

    fix

--- templates-research-pages/r-aos-dc.php
+++ templates-research-pages/r-aos-dc.php
@@ -47,7 +47,7 @@ foreach( $members as $group_members => $group_member_roles ) {
                 <article class="person-card">
                   <img class="person-card__image" src= "<?php echo esc_url( get_avatar_url( $user->ID ) );?>" alt="Headshot of Faculty Member">
                   <div class="person-card__info-wrapper">
-                    <h1 class="person-card__name"><a href="<?php get_author_link(true,$user->ID)?>"><span><?php echo esc_html($user->display_name);?></span></a></h1>
+                    <h1 class="person-card__name"><a style = "text-decoration: none;" href="<?php get_author_link(true,$user->ID)?>"><span><?php echo esc_html($user->display_name);?></span></a></h1>
                     <h2 class="person-card__department"><span><?php
                       global $wp_roles;
                       if (!empty($user->roles)){
